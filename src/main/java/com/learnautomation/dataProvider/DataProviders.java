5ca9357861548d8ba6354e2212289522de4c6577
Amalu <bindhu.amalu@gmail.com>
2024-02-16 21:45:43 +0530

    Framework pushing

---
+++ src/main/java/com/learnautomation/dataProvider/DataProviders.java
@@ -0,0 +1,17 @@
+package com.learnautomation.dataProvider;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviders 
+{
+
+	@DataProvider(name = "TestData")
+	public static Object [][] getData()
+	{
+		Object[][]arr=ExcelReader.getDataFromExcel("LoginData");
+		
+		return arr;
+	}
+
+	
+}
